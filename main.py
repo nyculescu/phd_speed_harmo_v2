809fe857f63d8fda3a4595a682597639bd2090ab
Catalin Niculescu <cn.cata@gmail.com>
2024-02-11 23:10:40 +0200

    check_for_TF and main func refactored

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import os
+
 def check_for_TF():
     import tensorflow as tf
     # Check TensorFlow GPU availability
     if tf.config.list_physical_devices('GPU'):
         print("GPU is available for TensorFlow")
+        print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+        return True
     else:
         print("GPU not available for TensorFlow")
-    
-    print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+        return False
 
 def check_for_PT():
     import torch
@@ -22,6 +25,11 @@ def check_for_PT():
         # device = torch.device("cpu")
 
 if __name__ == "__main__":
-    # check_for_PT()
-    check_for_TF()
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # Suppress TensorFlow logging except for errors
+    
+    if check_for_TF(): # check_for_PT()
+        print("System started running")
+    else:
+        print("System is not running")
+    
     # simulate_decentralized_nodes()
